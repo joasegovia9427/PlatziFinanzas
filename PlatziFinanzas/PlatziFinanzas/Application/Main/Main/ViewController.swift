8fe7b500a57a296bb77cf3144d5aff38af5c5f9e
Joaquin Segovia <joasegovia9427@gmail.com>
2022-04-13 22:35:09 -0300

    Inicio del proyecto con storyboard

---
+++ PlatziFinanzas/PlatziFinanzas/Application/Main/Main/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlatziFinanzas
+//
+//  Created by Joaquin Segovia on 13/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
